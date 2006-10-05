9a46a49289d29296be9e0eea5d9ee1a183ca4ff9
tarlen <tarlen@add21d40-e714-0410-9765-923e0770f407>
2006-10-05 03:44:45 +0000

    Updated default news page.
    git-svn-id: http://svn.code.sf.net/p/rpgwebprofiler/code/trunk@109 add21d40-e714-0410-9765-923e0770f407

--- root_profiler/templates/news.php
+++ root_profiler/templates/news.php
@@ -1,13 +1,21 @@
 <?php
 /*
- * news.php 
- * 
+ * news.php
+ *
  * Default news file for RPG Web Profiler
- * 
+ *
  * Created on Jun 28, 2006
  */
 ?>
 <h1>RPG Web Profiler News</h1>
+
+<h2>October 2006 - v0.5.0 Development</h2>
+
+<ul>
+<li>Major change to database access code.</li>
+<li>Extended security to allow 3rd party authentication.</li>
+<li>Added authentication module for phpBB v2 (Requires PHP 4).</li>
+</ul>
 
 <h2>1 August 2006 - v0.4.0 Released</h2>
 
@@ -73,7 +81,7 @@ ability to change details such as whether or not the character is public.</p>
 </ul>
 
 <p>The first two admin options listed above, along with the previous renaming of
-the system configuration file within the Subversion repository are intended to 
+the system configuration file within the Subversion repository are intended to
 make future upgrade paths easier. Ideally, you should be able to extract a new
 version of the application into the install directory without then having to
 re-configure the application for your site.</p>
