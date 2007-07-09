d3d551a22f3c4462f2f57bb5088352e6dcbd30bc
tarlen <tarlen@add21d40-e714-0410-9765-923e0770f407>
2007-07-09 06:24:29 +0000

    Updated SID class for faster queries
    git-svn-id: http://svn.code.sf.net/p/rpgwebprofiler/code/trunk@146 add21d40-e714-0410-9765-923e0770f407

--- root_profiler/engine/sid.class.php
+++ root_profiler/engine/sid.class.php
@@ -129,12 +129,6 @@
 		     $TABLE_CHARS, $name, $id,
 		     $TABLE_CHARS, $TABLE_CAMPAIGNS, $name, $id,
 		     $TABLE_OWNERS, $name, $id);
-      $sql = sprintf("select c.id as id from %s c where c.owner = '%s' and c.id = %d ".
-		     "union select c.id as id from %s c, %s n where c.campaign = n.id and n.owner = '%s' and c.id = %d ".
-		     "union select cid as id from %s where pname = '%s' and cid = %d ",
-		     $TABLE_CHARS, $name, $id,
-		     $TABLE_CHARS, $TABLE_CAMPAIGNS, $name, $id,
-		     $TABLE_OWNERS, $name, $id);
 
       $res = $rpgDB->query($sql);
       if( !$res ) {
