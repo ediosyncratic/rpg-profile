96627686029f53c1b59653e32491b93fbbf8a0be
tarlen <tarlen@add21d40-e714-0410-9765-923e0770f407>
2008-07-09 21:44:05 +0000

    Added 0.7.0 SQL to add 4e character sheet
    git-svn-id: http://svn.code.sf.net/p/rpgwebprofiler/code/trunk@187 add21d40-e714-0410-9765-923e0770f407

---
+++ root_profiler/update-mysqldb-0.6.0-0.7.0.sql
@@ -0,0 +1,7 @@
+
+-----------------------------------------------------------------
+-- Version 0.7.0
+-----------------------------------------------------------------
+
+-- Add 4e character sheet
+INSERT INTO sheet_templates (name, filename) VALUES ('d20 4e', 'standard/v4.php');
