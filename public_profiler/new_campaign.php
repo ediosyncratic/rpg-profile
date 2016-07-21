cc41419e5ec895e2fa5c43e9c3a133b6e711c675
Julien Picalausa <jpicalau@elmandria.com>
2016-07-21 19:03:31 +0000

    Removed superfluous use of incompatible last_insert_id()

--- public_profiler/new_campaign.php
+++ public_profiler/new_campaign.php
@@ -34,20 +34,8 @@
   if ($rpgDB->num_rows() != 1)
     __printFatalErr("Failed to update campaign list.", __LINE__, __FILE__);
   
-  // Get the character's id (the character should be the most recent character
-  // edited by this profile, and just to be sure, we restrict the select by
-  // cname as well).
-  $_r = $rpgDB->query(sprintf("select last_insert_id() as id from %s where owner='%s'",
-    $TABLE_CAMPAIGNS,
-    addslashes($sid->GetUserName())));
-  if (!$_r)
-    __printFatalErr("Failed to query database for new campaign id.", __LINE__, __FILE__);
-  $r = $rpgDB->fetch_row($_r);
-  $campaignID = $r['id'];
-
   // Everything should be fine, generate the success message.
   $title = 'New Campaign';
-  $id = $campaignID;
   $success = true;
   draw_page('new_campaign.php');
 ?>
